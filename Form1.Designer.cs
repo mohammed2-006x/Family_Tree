6e26b61e3d0339a456f0af458d574dd30ec356f3
Mohammed Taher <mohammedtaher2.006x@gmail.com>
2026-05-12 01:41:39 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnAddRoot = new System.Windows.Forms.Button();
+            this.btnAddChild = new System.Windows.Forms.Button();
+            this.btnClearTree = new System.Windows.Forms.Button();
+            this.btnRemoveChecked = new System.Windows.Forms.Button();
+            this.btnRemoveSelected = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Boy.png");
+            this.imageList1.Images.SetKeyName(1, "Girl.png");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 40F);
+            this.label1.Location = new System.Drawing.Point(397, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Family Tree";
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(38, 188);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(629, 491);
+            this.treeView1.TabIndex = 1;
+            // 
+            // btnAddRoot
+            // 
+            this.btnAddRoot.Font = new System.Drawing.Font("Algerian", 17F);
+            this.btnAddRoot.Location = new System.Drawing.Point(687, 364);
+            this.btnAddRoot.Name = "btnAddRoot";
+            this.btnAddRoot.Size = new System.Drawing.Size(289, 49);
+            this.btnAddRoot.TabIndex = 2;
+            this.btnAddRoot.Text = " Add Root";
+            this.btnAddRoot.UseVisualStyleBackColor = true;
+            this.btnAddRoot.Click += new System.EventHandler(this.btnAddRoot_Click);
+            // 
+            // btnAddChild
+            // 
+            this.btnAddChild.Font = new System.Drawing.Font("Algerian", 17F);
+            this.btnAddChild.Location = new System.Drawing.Point(998, 364);
+            this.btnAddChild.Name = "btnAddChild";
+            this.btnAddChild.Size = new System.Drawing.Size(289, 49);
+            this.btnAddChild.TabIndex = 3;
+            this.btnAddChild.Text = " ADD CHILD";
+            this.btnAddChild.UseVisualStyleBackColor = true;
+            this.btnAddChild.Click += new System.EventHandler(this.btnAddChild_Click);
+            // 
+            // btnClearTree
+            // 
+            this.btnClearTree.Font = new System.Drawing.Font("Algerian", 17F);
+            this.btnClearTree.Location = new System.Drawing.Point(850, 619);
+            this.btnClearTree.Name = "btnClearTree";
+            this.btnClearTree.Size = new System.Drawing.Size(289, 49);
+            this.btnClearTree.TabIndex = 4;
+            this.btnClearTree.Text = " clear tree";
+            this.btnClearTree.UseVisualStyleBackColor = true;
+            this.btnClearTree.Click += new System.EventHandler(this.btnClearTree_Click);
+            // 
+            // btnRemoveChecked
+            // 
+            this.btnRemoveChecked.Font = new System.Drawing.Font("Algerian", 17F);
+            this.btnRemoveChecked.Location = new System.Drawing.Point(850, 547);
+            this.btnRemoveChecked.Name = "btnRemoveChecked";
+            this.btnRemoveChecked.Size = new System.Drawing.Size(289, 49);
+            this.btnRemoveChecked.TabIndex = 5;
+            this.btnRemoveChecked.Text = " Remove checked ";
+            this.btnRemoveChecked.UseVisualStyleBackColor = true;
+            this.btnRemoveChecked.Click += new System.EventHandler(this.btnRemoveChecked_Click);
+            // 
+            // btnRemoveSelected
+            // 
+            this.btnRemoveSelected.Font = new System.Drawing.Font("Algerian", 17F);
+            this.btnRemoveSelected.Location = new System.Drawing.Point(850, 475);
+            this.btnRemoveSelected.Name = "btnRemoveSelected";
+            this.btnRemoveSelected.Size = new System.Drawing.Size(289, 49);
+            this.btnRemoveSelected.TabIndex = 6;
+            this.btnRemoveSelected.Text = " Remove Selected";
+            this.btnRemoveSelected.UseVisualStyleBackColor = true;
+            this.btnRemoveSelected.Click += new System.EventHandler(this.btnRemoveSelected_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Tahoma", 17F);
+            this.txtName.Location = new System.Drawing.Point(891, 182);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(289, 42);
+            this.txtName.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 17F);
+            this.label2.Location = new System.Drawing.Point(761, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = " Name : ";
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Checked = true;
+            this.rbMale.Font = new System.Drawing.Font("Algerian", 16.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMale.Location = new System.Drawing.Point(891, 256);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(108, 35);
+            this.rbMale.TabIndex = 9;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Font = new System.Drawing.Font("Algerian", 16.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFemale.Location = new System.Drawing.Point(891, 298);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(140, 35);
+            this.rbFemale.TabIndex = 10;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(1312, 700);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnRemoveSelected);
+            this.Controls.Add(this.btnRemoveChecked);
+            this.Controls.Add(this.btnClearTree);
+            this.Controls.Add(this.btnAddChild);
+            this.Controls.Add(this.btnAddRoot);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnAddRoot;
+        private System.Windows.Forms.Button btnAddChild;
+        private System.Windows.Forms.Button btnClearTree;
+        private System.Windows.Forms.Button btnRemoveChecked;
+        private System.Windows.Forms.Button btnRemoveSelected;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddRoot_Click(object sender, EventArgs e)
+        {
+            txtName.Text = txtName.Text.Trim();
+
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Please enter a name.");
+                return;
+            }
+
+            int IndexOfPhoto = (rbMale.Checked) ? 0 : 1;
+
+
+            TreeNode Node = new TreeNode(txtName.Text, IndexOfPhoto, IndexOfPhoto);
+
+            treeView1.Nodes.Add(Node);
+
+            txtName.Text = "";
+
+
+        }
+
+        private void btnAddChild_Click(object sender, EventArgs e)
+        {
+            txtName.Text = txtName.Text.Trim();
+
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("Please enter a name.");
+                return;
+            }
+
+
+            if (treeView1.Nodes.Count == 0)
+            {
+                MessageBox.Show("There is No Father to Add a Child to"); return;
+
+            }
+
+            if (treeView1.SelectedNode == null)
+            {
+                MessageBox.Show("Selected Father To Add Child"); return;
+            }
+
+
+            int IndexOfPhoto = (rbMale.Checked) ? 0 : 1;
+
+
+            TreeNode Node = new TreeNode(txtName.Text, IndexOfPhoto, IndexOfPhoto);
+
+            treeView1.SelectedNode.Nodes.Add(Node);
+
+            txtName.Text = "";
+
+
+
+
+        }
+
+        private void btnRemoveSelected_Click(object sender, EventArgs e)
+        {
+
+            if (treeView1.SelectedNode == null)
+            {
+                MessageBox.Show("Please select a node to remove.");
+                return;
+            }
+
+            treeView1.SelectedNode.Remove();
+        }
+
+        private void btnRemoveChecked_Click(object sender, EventArgs e)
+        {
+          RemoveCheckedNodes(treeView1.Nodes);
+        }
+
+        private void RemoveCheckedNodes(TreeNodeCollection nodes)
+        {
+            for (int i = nodes.Count - 1; i >= 0; i--)
+            {
+                if (nodes[i].Nodes.Count>0)
+                {
+                    RemoveCheckedNodes(nodes[i].Nodes);
+                }
+
+                if(nodes[i].Checked)
+                nodes.RemoveAt(i);
+                
+            }
+        }
+
+        private void btnClearTree_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+        }
+    }
+}
